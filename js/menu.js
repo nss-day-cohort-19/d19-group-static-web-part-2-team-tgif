413f234788a15333da1c6cd54fe87d9e3efe3867
Andrew Rock <Andrew Rock>
2017-04-13 19:24:58 -0500

    Raw Setup of Docs

---
+++ js/menu.js
@@ -0,0 +1,22 @@
+console.log("menu.js loaded");
+/* menu.js creates an array of products and populates the menu.html pag */
+
+/*  Menu Object */
+
+
+
+
+
+
+
+
+/*  Menu Population */
+
+
+
+
+
+
+
+
+
